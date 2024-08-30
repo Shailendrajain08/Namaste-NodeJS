37033a406d8749937f941805234bbad086758a7e
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-08-31 00:18:25 +0530

    Commit till import export

---
+++ app.js
@@ -0,0 +1,8 @@
+require("./xyz"); //one module into another
+const calculateSum = require("./sum")
+
+var myName = "Shailendra Jain"
+
+console.log(myName)
+
+calculateSum(10, 2)
---
+++ sum.js
@@ -0,0 +1,11 @@
+// Modules Protectes their variables and functions from leaking
+
+
+
+function calculateSum (a, b) {
+    const sum = a + b;
+
+    console.log(sum)
+}
+
+module.exports = calculateSum
